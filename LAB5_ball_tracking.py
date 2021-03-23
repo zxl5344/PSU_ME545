75dcf52d380aed732cc088bdc12d627e93109c2c
pi <zxl5344@psu.edu>
2021-03-23 17:03:23 -0400

    lab5

--- LAB5_ball_tracking.py
+++ LAB5_ball_tracking.py
@@ -4,34 +4,48 @@ import numpy as np
 import imutils
 import cv2
 import time
+import string 
+import serial
+from gpiozero import Servo
+from time import sleep
 
 
+#serial bud rate
+ser = serial.Serial('/dev/ttyACM0',9600)
+ser.flush()
+
+'''
+for i in range(0,10):
+
+	ser.write(chr(255))
+	sleep(0.1)
+'''
+
+ser.flush()
+angle = 90
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
 greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+greenUpper = (120, 240, 240)
 
 #get camera
 camera = cv2.VideoCapture(0)
+camera.set(4,480)
+camera.set(4,360)
 
-
-
-#Reading the time in the begining of the video.
-start = time.time()
+start_time = time.time
 
 # keep looping
+
 while True:
+
 	# grab the current frame
 	(grabbed, frame) = camera.read()
 	
-	#Reading The Current Time
-	current_time = time.time() - start
 
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
-	frame = imutils.resize(frame, width=1000)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 	# construct a mask for the color "green", then perform
@@ -43,9 +57,7 @@ while True:
 
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)[-2]
-	center = None
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
 
 	# only proceed if at least one contour was found
 	if len(cnts) > 0:
@@ -54,31 +66,35 @@ while True:
 		# centroid
 		c = max(cnts, key=cv2.contourArea)
 		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 		
 
 		# only proceed if the radius meets a minimum size
 		if (radius < 300) & (radius > 10 ) : 
-			# draw the circle and centroid on the frame,
+			# draw the circle on the frame,
 			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)		
+			cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)		
 
 
 			# otherwise, compute the thickness of the line and
 			# draw the connecting lines
 			thickness = 10
-			cv2.line(frame, (500,400), (int(x),int(y)), (0, 0, 255), thickness)
+			cv2.line(frame, (310,240), (int(x),int(y)), (0, 0, 255), thickness)
 
            		 # display coordinate data
 			font = cv2.FONT_HERSHEY_SIMPLEX
+			
+			angle = int(np.arctan(-(x-310)/302.22)*180/3.14+90)
 			cv2.putText(frame,str(int(x)) + ',' + str(int(y)) ,(int(x), int(y)), font, 1,(255,255,255),2)
+			
 
 
 	# show the frame to our screen
 	cv2.imshow("Frame", frame)
+	print(angle)
+	ser.write(chr(angle))
+	line = ser.readline().decode('utf-8').rstrip()
+	print('Arduino'+line)
+
 	key = cv2.waitKey(1) & 0xFF
 
 	# if the 'q' key is pressed, stop the loop
